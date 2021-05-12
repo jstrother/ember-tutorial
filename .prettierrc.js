5c96aae9a711b8df91eae978fb816eb63fec0d1a
Jim Strother <jim.strother@gmail.com>
2021-05-11 21:46:32 -0500

    Add tests

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,11 @@
 
 module.exports = {
   singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: 'always',
 };
