791cfe6fd09a1eff9726323ae1f1eae4b4a8cb7d
Jim Strother <jim.strother@gmail.com>
2021-05-11 22:10:19 -0500

    Add NavBar test & component

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -3,24 +3,18 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | jumbo', function(hooks) {
+module('Integration | Component | jumbo', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders the content inside a jumbo header with a tomster', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Jumbo />`);
-
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`<Jumbo>Hello World</Jumbo>`);
 
     // Template block usage:
-    await render(hbs`
-      <Jumbo>
-        template block text
-      </Jumbo>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('Hello World');
+    assert.dom('.jumbo .tomster').exists();
   });
 });
